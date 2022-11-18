5b3c40d4d0754e2124a0db96f26e94483dda4fba
WAJDA9 <wajdizakhama9@gmail.com>
2022-11-18 13:55:11 +0100

    First commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import products from './products.js'
+const app = express()
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+app.get('/' , (req,res) => {
+    res.send('API is runnning ...')
+})
+
+app.get('/api/product' , (req,res) => {
+    res.json(products)
+})
+
+app.get('/api/products/:id' , (req,res) => {
+    const product = products.find((p) => p._id === req.params.id)
+    res.json(product)
+})
+const port = process.env.PORT || 5000
+app.listen(port,console.log(`server running in ${process.env.NODE_ENV} mode on port ${port}`))
